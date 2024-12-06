0a1c541511737be37435bc0601876f20b86058a0
Simone-Andreotti <simone.andreotti.98@gmail.com>
2024-12-06 15:54:14 +0100

    fix(ios): fixed tab position in ios

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -19,10 +19,6 @@ export default function TabLayout() {
         tabBarButton: HapticTab,
         tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
           default: {},
         }),
       }}>
