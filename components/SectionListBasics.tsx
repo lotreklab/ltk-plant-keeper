c4a93cdef693c6977e7c47b29e9c04be0fe1466f
Leonardo Fais <leonardo@lotrek.it>
2025-01-10 16:59:20 +0100

    feat: wip on section list style

--- components/SectionListBasics.tsx
+++ components/SectionListBasics.tsx
@@ -9,28 +9,71 @@ const styles = StyleSheet.create({
     paddingTop: 22,
     width: '100%',
   },
+  ListHeader: {
+    paddingTop: 25,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 50,
+    fontSize: 24,
+    fontWeight: 'bold',
+    backgroundColor: '#2DDA93',
+    color: '#FFFFFF',
+    marginBottom: -25,
+  },
   sectionHeader: {
     paddingTop: 2,
     paddingLeft: 10,
     paddingRight: 10,
     paddingBottom: 2,
-    fontSize: 14,
+    fontSize: 18,
     fontWeight: 'bold',
-    backgroundColor: 'rgba(247,247,247,1.0)',
+    color: '#2DDA93',
   },
   item: {
     padding: 10,
-    fontSize: 18,
+    fontSize: 15,
     height: 44,
+    color: '#6A6F7D',
+    textTransform: 'uppercase',
   },
   textInputStyle: {
-    height: 40,
-    borderWidth: 1,
-    paddingLeft: 20,
-    margin: 5,
-    borderColor: '#009688',
+    position: 'relative',
+    display: 'flex',
+    justifyContent: 'center',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    height: 50,
+    minWidth: 200,
+    width: '90%',
+    /* borderWidth: 2,
+    borderColor: '#009688', */
     backgroundColor: '#FFFFFF',
+    borderRadius: 90,
+    paddingTop: 16,
+    paddingBottom: 16,
+    paddingLeft: 24,
+    paddingRight: 24,
+    color: '#D2D2D2',
   },
+  textInputActive: {
+    position: 'relative',
+    display: 'flex',
+    justifyContent: 'center',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    height: 50,
+    minWidth: 200,
+    width: '90%',
+    /* borderWidth: 2,
+    borderColor: '#009688', */
+    backgroundColor: '#FFFFFF',
+    borderRadius: 90,
+    paddingTop: 16,
+    paddingBottom: 16,
+    paddingLeft: 24,
+    paddingRight: 24,
+    color: '#000000', // Change text color when typing
+  }
 });
 
 export function SectionListBasics() {
@@ -131,20 +174,23 @@ export function SectionListBasics() {
   };
   return (
     <View style={styles.container}>
-        <TextInput
-          style={styles.textInputStyle}
-          onChangeText={(text) => searchFilterFunction(text)}
-          value={search}
-          underlineColorAndroid="transparent"
-          placeholder="Search Here"
-        />
+      <Text style={styles.ListHeader}>List Title</Text>
+
+      <TextInput
+      style={search ? styles.textInputActive : styles.textInputStyle}
+      onChangeText={(text) => searchFilterFunction(text)}
+      value={search}
+      underlineColorAndroid="transparent"
+      placeholder="Search Here"
+      />
       <SectionList
-        sections={filteredDataSource}
-        renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
-        renderSectionHeader={({section}) => (
-          <Text style={styles.sectionHeader}>{section.title}</Text>
-        )}
-        keyExtractor={item => `basicListEntry-${item}`}
+      sections={filteredDataSource}
+      renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+      renderSectionHeader={({section}) => (
+        <Text style={styles.sectionHeader}>{section.title}</Text>
+      )}
+      keyExtractor={item => `basicListEntry-${item}`}
+      renderSectionFooter={() => <View style={{height: 20}} />}
       />
     </View>
   );
